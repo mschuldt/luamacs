70093d059770c46847b6cb3bf8d5a0b3c35086d0
mschuldt <mbschuldt@gmail.com>
2014-11-07 21:26:37 -0800

    __pairs method for cons references

--- lisp/luamacs/luamacs.el
+++ lisp/luamacs/luamacs.el
@@ -7,4 +7,17 @@
   (interactive)
   (lua-eval (buffer-string)))
 
+(lua-eval "function lisp_cons_metatable.__pairs (tbl)
+   local len = #tbl
+   local i = 0;
+   return function ()
+      local ii
+      while i < len do
+         ii = i
+         i = i + 1
+         return ii, tbl[ii]
+      end
+   end
+end")
+
 (provide 'luamacs)
