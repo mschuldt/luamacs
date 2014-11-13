b243a28cbf6f338e2564fb256e8177b7c1d5d75a
mschuldt <mbschuldt@gmail.com>
2014-11-12 23:16:43 -0800

    __pairs method for vector references

--- lisp/luamacs/luamacs.el
+++ lisp/luamacs/luamacs.el
@@ -20,4 +20,17 @@
    end
 end")
 
+(lua-eval "function lisp_vector_metatable.__pairs (tbl)
+   local len = #tbl
+   local i = 0;
+   return function ()
+      local ii
+      while i < len do
+         ii = i
+         i = i + 1
+         return ii, tbl[ii]
+      end
+   end
+end")
+
 (provide 'luamacs)
